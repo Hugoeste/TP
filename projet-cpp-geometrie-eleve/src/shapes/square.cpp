442e546de8c750d894195e09578e126ff165c518
AxelFrd <axel.feraud@etu-upsaclay.fr>
2025-05-07 14:36:08 +0200

    Update square.cpp

--- projet-cpp-geometrie-eleve/src/shapes/square.cpp
+++ projet-cpp-geometrie-eleve/src/shapes/square.cpp
@@ -1,4 +1,34 @@
 #include "point.hpp"
 #include "shapes/square.hpp"
-
 Square::Square(Point P, Point R) : A(P), C(R) {};
+Square::double side(){
+  double z;
+  z=P.x-R.x;
+  if(z<0){
+  z=-z;
+  return z;
+}
+Square::double perimeter(){
+    return side()*4;
+}
+Square::double area(){
+    return side()*side();
+}
+Square::point center(){
+  point w;
+  if(P.x<R.x){
+    w.x=P.x+(R.x-P.x)/2;
+  }
+  else{
+    w.x=R.x+(P.x-R.x)/2;
+  }
+  if(P.y<R.y){
+    w.x=P.y+(R.y-P.y)/2;
+  }
+  else{
+    w.y=R.y+(P.y-R.y)/2;
+  }
+  return w;
+}
+Square::void draw(){
+  
