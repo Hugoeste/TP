2dd693bbed0f373f1fc6fc7fa0816b25d8aea174
yadac1 <yanis.da-costa@etu-upsaclay.fr>
2025-05-07 14:36:00 +0200

    Update circle.cpp
    Ajout translation

--- projet-cpp-geometrie-eleve/src/shapes/circle.cpp
+++ projet-cpp-geometrie-eleve/src/shapes/circle.cpp
@@ -2,13 +2,26 @@
 #include "shapes/circle.hpp"
 #include <cmath>
 double pi = M_PI;
+
 Circle::Circle(double r, Point c) : radius(r), center(c) {};
+
 double Circle::circumference(){
   return 2*pi*radius;
 }
+
 double Circle::area(){
   return pi*rad*rad;
 }
+
 void Circle::draw(){
+  t.circle(radius);
+}
+
+void Circle::translate(Point T){
+  center.x += T.x;
+  center.y += T.y;
+}
+  
+  
   
   
