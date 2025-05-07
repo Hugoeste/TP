775c37b66d946f850af9eafefd63360eb0680318
yadac1 <yanis.da-costa@etu-upsaclay.fr>
2025-05-07 14:24:54 +0200

    Update circle.cpp
    Aire + périmètre

--- projet-cpp-geometrie-eleve/src/shapes/circle.cpp
+++ projet-cpp-geometrie-eleve/src/shapes/circle.cpp
@@ -1,4 +1,14 @@
 #include "point.hpp"
 #include "shapes/circle.hpp"
-
+#include <cmath>
+double pi = M_PI;
 Circle::Circle(double r, Point c) : radius(r), center(c) {};
+double Circle::circumference(){
+  return 2*pi*radius;
+}
+double Circle::area(){
+  return pi*rad*rad;
+}
+void Circle::draw(){
+  
+  
