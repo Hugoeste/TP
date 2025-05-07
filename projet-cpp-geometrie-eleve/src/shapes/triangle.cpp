e3305efa8db2ede6a3e57665c117869c94597a60
LucasCognard <lucas.cognard@etu-upsaclay.fr>
2025-05-07 14:40:57 +0200

    Update triangle.cpp
    périmètre
    aire

--- projet-cpp-geometrie-eleve/src/shapes/triangle.cpp
+++ projet-cpp-geometrie-eleve/src/shapes/triangle.cpp
@@ -2,3 +2,29 @@
 #include "shapes/triangle.hpp"
 
 Triangle::Triangle(Point P, Point Q, Point R) : A(P), B(Q), C(R) {}
+
+double Triangle::perimeter(){
+  return A.distance(B)+B.distance(C)+C.distance(A);
+}
+
+double absolue (double a){
+  if(a<0){
+    return a*-1;
+  }
+  return a;
+}
+
+double Triangle::area(){
+  return 0.5*absolue(A.x*(B.y-C.y) + B.x*(C.y-A.y) + C.x*(A.y-B.y))
+}
+
+Point Triangle::center(){
+
+
+}
+  
+  
+
+
+
+
