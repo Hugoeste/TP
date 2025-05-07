5e5a0bc294eec7c1eecfe2ccbf23ee7ffabae496
yadac1 <yanis.da-costa@etu-upsaclay.fr>
2025-05-07 14:46:34 +0200

    Update circle.cpp
    Ajout fonction valeur absolue

--- projet-cpp-geometrie-eleve/src/shapes/circle.cpp
+++ projet-cpp-geometrie-eleve/src/shapes/circle.cpp
@@ -1,8 +1,14 @@
 #include "point.hpp"
 #include "shapes/circle.hpp"
 #include <cmath>
+double abs(x){
+  if (x<0){
+    x = -x;
+  }
+return x;
+}
 double pi = M_PI;
-
+double epsilon = 0.00000000001;
 Circle::Circle(double r, Point c) : radius(r), center(c) {};
 
 double Circle::circumference(){
@@ -21,6 +27,11 @@ void Circle::translate(Point T){
   center.x += T.x;
   center.y += T.y;
 }
+void Circle::resize(double ratio){
+  return ratio*radius;
+}
+bool Circle::equals(Circle circle){
+  bool egal = 
   
   
   
