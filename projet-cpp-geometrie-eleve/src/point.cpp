c36824698ae7e43155976e1aef46f6958770328f
LucasCognard <lucas.cognard@etu-upsaclay.fr>
2025-05-15 09:12:52 +0200

    Update point.cpp

--- projet-cpp-geometrie-eleve/src/point.cpp
+++ projet-cpp-geometrie-eleve/src/point.cpp
@@ -3,6 +3,6 @@
 
 Point::Point(double x_val, double y_val) : x(x_val), y(y_val) {}
 
-double Point::distance(Point B = Point()) {
+double Point::distance(Point B) {
   return sqrt((B.x-x)*(B.x-x)+(B.y-y)*(B.y-y));
 }
